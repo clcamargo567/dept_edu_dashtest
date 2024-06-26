c997f40d300bfd1feabd5495a9c7531655bbf01e
clcamargo567 <clcamargoaf@gmail.com>
2024-06-26 19:49:38 -0400

    update render autp deploy enabled

--- src/app.py
+++ src/app.py
@@ -37,7 +37,7 @@ SIDEBAR_STYLE = {
     "bottom": 0,
     "width": "16rem",
     "padding": "2rem 1rem",
-    "background-color": "#f8f9fa",
+    "background-color": "#21c7ef",
 }
 
 # the styles for the main content position it to the right of the sidebar and
